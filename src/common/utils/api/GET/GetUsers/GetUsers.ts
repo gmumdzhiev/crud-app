74ca8adc54b2f3029afcac93774b27f57c4dfe67
Georgi Mumdzhiev <george.mumdzhiev@gmail.com>
2025-03-06 13:36:15 +0100

    setting users to the firebase database

--- src/common/utils/api/GET/GetUsers/GetUsers.ts
+++ src/common/utils/api/GET/GetUsers/GetUsers.ts
@@ -1,21 +1,38 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import { IGetUsers, IGetUsersErrorHandle } from "./interfaces/IGetUsers.ts";
+import { collection, getDocs, addDoc } from "firebase/firestore";
+import { db } from "../../../../../../firebaseConfig.ts";
 
-import {IGetUsers, IGetUsersErrorHandle} from "./interfaces/IGetUsers.ts";
-
-export const GetUsers = createAsyncThunk<IGetUsers[], void, { rejectValue: IGetUsersErrorHandle }>(
+export const GetUsers = createAsyncThunk<
+    IGetUsers[],
+    void,
+    { rejectValue: IGetUsersErrorHandle }
+>(
     "avatars/getUsers",
     async (_, { rejectWithValue }) => {
         try {
-            const response = await fetch(`https://jsonplaceholder.typicode.com/users`, {
-                method: "GET",
-            });
+            const querySnapshot = await getDocs(collection(db, "users"));
+            const firebaseUsers: IGetUsers[] = querySnapshot.docs.map(
+                (doc) => doc.data() as IGetUsers
+            );
 
+            const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
             if (!response.ok) {
-                throw new Error("Failed to fetch avatars");
+                throw new Error("Failed to fetch users from JSONPlaceholder");
             }
 
-            const data: IGetUsers[] = await response.json();
-            return data;
+            const jsonPlaceholderUsers: IGetUsers[] = await response.json();
+
+            const existingUserIds = new Set(firebaseUsers.map((user) => user.id));
+            const newUsers = jsonPlaceholderUsers.filter(
+                (user) => !existingUserIds.has(user.id)
+            );
+
+            for (const newUser of newUsers) {
+                await addDoc(collection(db, "users"), newUser);
+            }
+
+            return [...firebaseUsers, ...newUsers];
         } catch (error) {
             return rejectWithValue(error.message);
         }
